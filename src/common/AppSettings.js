afe13771bf915424c9235000b07853cf9d6d2ce2
Jeevan Jose <josej@Jeevans-MBP.djmesh>
2018-08-01 20:15:52 +1000

    Updated AppSettings with itunes lookup url and cors url

--- src/common/AppSettings.js
+++ src/common/AppSettings.js
@@ -1,3 +1,6 @@
 const ITUNES_URL = 'https://itunes.apple.com/search?media=podcast&entity=podcast&limit=50&term=';
+const ITUNES_LOOKUP_URL = 'https://itunes.apple.com/lookup?id=';
 
-export { ITUNES_URL };
+const CORS_URL = '';
+
+export { ITUNES_URL, ITUNES_LOOKUP_URL, CORS_URL };
