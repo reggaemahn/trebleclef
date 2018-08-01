4bf1bfcc628e5b1ef7b9868374a0b32a82de1cc9
Jeevan Jose <josej@Jeevans-MBP.djmesh>
2018-08-01 20:12:15 +1000

    Updated SearchService 1. Replaced axios with fetch 2. Added method to lookup podcast from itunes, get the feedUrl, parse the xml and return podcast details

--- src/common/services/SearchService.js
+++ src/common/services/SearchService.js
@@ -1,27 +1,81 @@
-import axios  from 'axios';
 import * as AppSettings from '../AppSettings';
+import * as XmlJs from 'xml-js';
 
-export class SearchService{
-    constructor(searchTerm) {
-        this.searchTerm = searchTerm;
-    }
+export class SearchService {
 
-    async findPodcastEpisodes(){
-        const uri = encodeURI(`${ AppSettings.ITUNES_URL } ${ this.searchTerm }`);
-        const search = await axios.get(uri, 
-        {
-            headers: {"Access-Control-Allow-Origin": "*"}
-        });
-        const results = await search.data;
+    async findPodcasts(searchTerm) {
+        const uri = encodeURI(AppSettings.CORS_URL + AppSettings.ITUNES_URL + searchTerm);
+        const results = await (await fetch(uri)).json();
 
-        const formattedResults = results.results.map((episode) => ({
-            trackId: episode.trackId,
-            name: episode.collectionName,
+        const formattedResults = results.results.map((podcast) => ({
+            trackId: podcast.trackId,
+            name: podcast.collectionName,
             description: '',
-            thumbnailUrl: episode.artworkUrl100,
-            artist: episode.artistName
+            thumbnailUrl: podcast.artworkUrl100,
+            artist: podcast.artistName,
+            feedUrl: podcast.feedUrl
         }));
 
         return formattedResults;
+    }
+
+    async findPodcast(podcastId) {
+        const uri = encodeURI(AppSettings.CORS_URL + AppSettings.ITUNES_LOOKUP_URL + podcastId);
+
+        const search = await (await fetch(uri)).json();
+        const result = search.results[0];
+
+        return {
+            trackId: result.trackId,
+            name: result.collectionName,
+            description: '',
+            thumbnailUrl: result.artworkUrl100,
+            artist: result.artistName,
+            feedUrl: result.feedUrl
+        };
+    }
+
+    async getPodcastDetails(podcastId) {
+        const podcast = await this.findPodcast(podcastId);
+
+        const uri = encodeURI(AppSettings.CORS_URL + podcast.feedUrl);
+        const podcastFeedXml = await (await fetch(uri, {
+            headers: new Headers({
+                'Content-Type': 'application/rss+xml'
+            })
+        })).text();
+
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(podcastFeedXml, 'application/xml');
+        console.log(doc);
+        const episodeNodes = doc.querySelectorAll('rss > channel > item');
+
+        const episodes = [];
+
+        [...episodeNodes].map(episodeNode => {
+            const title = episodeNode.querySelector('title');
+            const pubDate = episodeNode.querySelector('pubDate');
+            const description = episodeNode.querySelector('description');
+            const audioUrl = episodeNode.querySelector('enclosure');
+            const audioFormat = episodeNode.querySelector('enclosure');
+
+            if(audioUrl && audioFormat){
+                episodes.push({
+                    title: title === null ? ' ' : title.textContent,
+                    pubDate: pubDate  === null ? ' ' : pubDate.textContent,
+                    description: description  === null ? ' ' : description.innerText,
+                    audioUrl: audioUrl.getAttribute('url'),
+                    audioFormat: audioFormat.getAttribute('type')
+                });
+            }
+        });
+
+        return {
+            title: doc.querySelector('rss > channel > title').textContent,
+            description: doc.querySelector('rss > channel > description').textContent,
+            imageUrl: doc.querySelector('rss > channel > image > url').textContent,
+            author: '',
+            episodes: episodes
+        };
     }
 }
