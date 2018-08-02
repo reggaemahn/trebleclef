282dd99af096e321a6327852a9cdd5e2a60c8b2b
Jeevan Jose <josej@Jeevans-MBP.djmesh>
2018-08-02 21:54:38 +1000

    Updated styling for Podcast Episode and added episode publish date

--- src/components/PodcastEpisode.js
+++ src/components/PodcastEpisode.js
@@ -3,8 +3,9 @@ import * as React from 'react';
 const PodcastEpisode = (props) => {
     return (
         <div className="box">
-            <p><b>{props.data.title}</b></p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.description }}></div>
+            <p className="is-size-5"><b>{props.data.title}</b></p>
+            <p>{props.data.pubDate}</p>
+            <div className="podcast-episode-description" dangerouslySetInnerHTML={{ __html: props.data.description }}></div>
             <div>
             <audio controls className="audio-control">
                 <source src={props.data.audioUrl} type='audio/mpeg' />
