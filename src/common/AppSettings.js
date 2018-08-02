e5fe0d0abddf22392686058f0f59a26ccc37eccc
Jeevan Jose <josej@Jeevans-MBP.djmesh>
2018-08-02 21:52:37 +1000

    Added itunes xmlns link

--- src/common/AppSettings.js
+++ src/common/AppSettings.js
@@ -1,6 +1,7 @@
 const ITUNES_URL = 'https://itunes.apple.com/search?media=podcast&entity=podcast&limit=50&term=';
 const ITUNES_LOOKUP_URL = 'https://itunes.apple.com/lookup?id=';
+const ITUNES_XMLNAMESPACE = 'http://www.itunes.com/dtds/podcast-1.0.dtd';
 
 const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
 
-export { ITUNES_URL, ITUNES_LOOKUP_URL, CORS_URL };
+export { ITUNES_URL, ITUNES_LOOKUP_URL, CORS_URL, ITUNES_XMLNAMESPACE };
